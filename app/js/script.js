ea4d9d089e0c6912e2e2bbe4832fed6abc226e9f
fabio neves <fabsalgadinho@gmail.com>
2021-12-08 20:35:38 +0000

    added hamburguer

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log('hello world!');
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener('click', function () {
+  console.log('click hamburger');
+
+  if (header.classList.contains('open')) {
+    //close hamburguer menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } 
+  else {
+    //open hamburguer menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');  
+    }
+});
