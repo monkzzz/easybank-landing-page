eb560f2e7a2a1bbc3fc7cff9efdaab64b1c31302
fabio neves <fabsalgadinho@gmail.com>
2021-12-11 22:52:58 +0000

    added mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,8 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener('click', function () {
   console.log('click hamburger');
@@ -8,13 +10,19 @@ btnHamburger.addEventListener('click', function () {
   if (header.classList.contains('open')) {
     //close hamburguer menu
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    body.classList.remove('no-scroll');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');  
+    });
   } 
   else {
     //open hamburguer menu
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');  
+    body.classList.add('no-scroll');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');  
+    });
     }
 });
